f46ba739b8d3776357e3d00d9f02b8373dc5be0c
Unknown <misskristinablinova@gmail.com>
2017-12-14 12:12:30 +0000

    Some small changes

--- GameServer/GameServer/Program.cs
+++ GameServer/GameServer/Program.cs
@@ -4,6 +4,7 @@ using System.Threading;
 using System.Text;
 using System.Net;
 
+
 namespace GameServer
 {
 
@@ -38,13 +39,34 @@ namespace GameServer
 		public UdpClient udpClient;
 		public int bufferSize = 1024;
 		public byte[] buffer = new byte[1024];
+
+		//Constructor
+		public UdpState()
+		{
+			udpClient = new UdpClient();
+			udpClient.EnableBroadcast = true;
+		}
 	}
+
+	//TEST Client
+	//class client
+	//{
+	//	UdpState udpState;
+	//	StateObject tcpState;
+	//	int id;
+	//}
 
 	/// <summary>
 	/// Main body of the server
 	/// </summary>
 	class MainClass
 	{
+		//--------------------------------------
+		//				USERS
+		//--------------------------------------
+
+		//client clientA;
+		//client clientB;
 
 		//--------------------------------------
 		//			LISTENING LOOP
@@ -229,6 +251,10 @@ namespace GameServer
 			//test
 			stateUdp.endPoint = (IPEndPoint) ePoint;
 			stateUdp.udpClient = uClient;
+
+			//Try
+			stateUdp.udpClient.EnableBroadcast = true;
+
 			stateUdp.workingSocket = udpSocket;
 
 			Console.WriteLine("Listening for messages udp");
